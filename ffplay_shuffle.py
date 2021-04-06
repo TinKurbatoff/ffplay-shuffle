d356940645be143a068160d9b059b5c8a3e03071
Constantine Kurbatoff <6782841@gmail.com>
2021-04-06 15:23:58 -0400

    proper comments added

--- ffplay_shuffle.py
+++ ffplay_shuffle.py
@@ -10,22 +10,23 @@ import pickle
 
 music_list_file = 'music_list.pickle' # list with music
 curr_pos_file = 'current_play_idx.pickle'
-delay = 5 # delay between tracks
+delay = 5 # delay between tracks in seconds
 
-### Read list of the music o create one
+### Read the list of music tracks or create one
 if path.isfile(music_list_file): 
     with open(music_list_file, 'rb') as f:
         music_list = pickle.load(f)
 else:
-    ## create a shuffled list 
+    ## ..or create a new shuffled list 
     mypath = './'
     _, _, filenames = next(walk(mypath))
     random.shuffle(filenames)
     music_list=[]
     for i in filenames:
         if i.endswith("mp3"):
+            # pick only mp3
             music_list.append(i)
-            # print(i)
+    # remember the new list        
     with open(music_list_file, 'wb') as f:
         pickle.dump(music_list, f)
 
@@ -35,7 +36,7 @@ if path.isfile(curr_pos_file):
         last_used_idx = int(f.read().strip())
         print('Skipped {} tracks...')
 else:
-    last_used_idx = 0 # if not — start from begining
+    last_used_idx = 0 # if not — start from the begining
 
 # play music from current dir
 dirpath = path.dirname(path.realpath(__file__))
@@ -43,22 +44,23 @@ dirpath = path.dirname(path.realpath(__file__))
 ### Playing from the last position
 for current_index in range(last_used_idx,len(music_list)):
     with open(curr_pos_file, 'w') as f:
+        # save current index
         f.write(f'{current_index}')
+    # use ffplay with suppressed stats and limiting window to 450x250
     command_line = ['ffplay','-autoexit', '-x','450', '-y', '250','-loglevel', 'error','-stats']
-    music_file = f'{dirpath}/{music_list[current_index]}'
+    music_file = f'{dirpath}/{music_list[current_index]}' # add path to filename
     command_line.append(music_file)
-    print(f"{current_index}. {music_list[current_index]}")
-    # print(f"Executing command: {command_line}")
-    # with open('playing.out', 'w') as f:
-    #     process = subprocess.Popen(command_line, stdout=f)
+    print(f"{current_index}. {music_list[current_index]}") # Show what track is playing
     process = subprocess.Popen(command_line,
                          stdout=subprocess.PIPE, 
                          stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
+    # FOR DEBUG PURPOSES: stdout, stderr = process.communicate()
+    # Delay before the next track and allow to break sequence (index saved!)
     with Progress(transient=True) as progress:
-        task1 = progress.add_task(f"[green]next in {delay} sec...", total=1000)
+        # beautiful progress bar
+        task1 = progress.add_task(f"[green]next in {delay} sec...", total=100)
         while not progress.finished:
-            progress.update(task1, advance=10)
+            progress.update(task1, advance=1)
             sleep(delay/100)   
     # break
 print('Bye!')
